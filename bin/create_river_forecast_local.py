c4ff0b0c1e39538e66b12398556388562e5adc6a
Max Nolte <1928876+maxnolte@users.noreply.github.com>
2020-08-30 18:01:33 +0200

    new script for local plotting

---
+++ bin/create_river_forecast_local.py
@@ -0,0 +1,10 @@
+from river_forecast.api_data_access import RivermapDataRetriever
+from river_forecast.forecast import SARIMAXForecast, NaiveForecast, LSTMForecast, LSTMSeq2SeqForecast, XGBForecast
+
+recent_data = RivermapDataRetriever().get_standard_dranse_data()
+# SARIMAXForecast().generate_prediction_plot(recent_data, show=True)
+# NaiveForecast().generate_prediction_plot(recent_data, show=True)
+# LSTMForecast().generate_prediction_plot(recent_data, show=True)
+# LSTMSeq2SeqForecast().generate_prediction_plot(recent_data, show=True)
+
+XGBForecast().generate_prediction_plot(recent_data, show=False, ci=True, save_png=True)
