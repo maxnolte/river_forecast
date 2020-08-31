1b860568a419897ea8e8cea9149497ad7789605a
Max Nolte <1928876+maxnolte@users.noreply.github.com>
2020-08-31 17:02:16 +0200

    plotted error metrics

--- compute_errors.py
+++ compute_errors.py
@@ -1,24 +1,8 @@
 from river_forecast.api_data_access import RivermapDataRetriever
 from river_forecast.forecast import SARIMAXForecast, NaiveForecast, LSTMForecast, LSTMSeq2SeqForecast, XGBForecast
 
-# recent_data = RivermapDataRetriever().get_standard_dranse_data()
-# SARIMAXForecast().generate_prediction_plot(recent_data, show=True)
-# NaiveForecast().generate_prediction_plot(recent_data, show=True)
-# LSTMForecast().generate_prediction_plot(recent_data, show=True)
-# LSTMSeq2SeqForecast().generate_prediction_plot(recent_data, show=True)
 
-xgb = XGBForecast()
+models = [SARIMAXForecast(), NaiveForecast(), LSTMForecast(), LSTMSeq2SeqForecast()]
 
-forecasts, real_values = xgb.compute_real_and_predicted_values_test_data(n_forecasts=20)
-
-print(forecasts.shape, real_values.shape)
-
-
-forecasts, real_values = xgb.get_real_and_predicted_values_test_data(recompute=False)
-
-print(forecasts.shape, real_values.shape)
-
-
-error_metrics = xgb.get_error_metrics(recompute=False, ci=60)
-
-print(error_metrics)
+for model in models:
+    model.get_real_and_predicted_values_test_data(recompute=False)
